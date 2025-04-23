d69fedd1da1ec8879e83a712f12ae183f0d7fa9f
DianaSReyes20 <dianasreyes20@gmail.com>
2025-04-23 12:53:00 -0500

    Creación del componente de resultados de encuestas "poll"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  polls = [
+    {
+      question: '¿Prefieres los gatos o los perros?',
+      image: 'https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg',
+      votes: [0, 5, 7],
+      voted: true,
+    },
+    {
+      question: '¿Cuál es el mejor mes para pasar las vacaciones?',
+      image: 'https://images.pexels.com/photos/1118448/pexels-photo-1118448.jpeg',
+      votes: [1, 6, 4],
+      voted: false,
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PollCreateComponent } from './poll-create/poll-create.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PollCreateComponent } from './poll-create/poll-create.component'
+import { PollComponent } from "./poll/poll.component";
 
 @NgModule({
-  declarations: [AppComponent, PollCreateComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, PollCreateComponent, PollComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
